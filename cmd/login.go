905a0f280ac2a2731a217d113f35e286f9aeb5e8
Pieter-Jan Briers <pieterjan.briers@level27.be>
2023-03-01 13:23:37 +0100

    Allow automating lvl login.
    New -u flag on the command allows passing username, and password prompt can also accept input piped in now.
    
    PL-8760

--- cmd/login.go
+++ cmd/login.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -31,16 +31,31 @@ import (
 )
 
 // loginCmd represents the login command
+var optLoginUsername string
 var loginCmd = &cobra.Command{
 	Use:   "login",
-	Short: "Login to CP4",
-	Long:  ``,
+	Short: "Log in to CP4",
+	Long: `Log in to CP4
+You will be prompted for your user name (usually email, like example@level27.be) and password.
+If --username is specified, user name will not be prompted, only password. This can be useful for scripts.
+
+After the first login, further logins will suggest you to re-use the previous username when logging in.
+`,
+	Example: `Log in manually:
+lvl login
+
+Log in automatically:
+cat password.txt | lvl login -u my.awesome.email@level27.be`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		var login l27.Login
-		username, password, _ := credentials()
+		username, password, err := loginPromptCredentials()
+		if err != nil {
+			return err
+		}
 
 		client := l27.NewAPIClient(apiUrl, "")
-		login, err := client.Login(username, password)
+
+		login, err = client.Login(username, password)
 		if err != nil {
 			return err
 		}
@@ -62,44 +77,48 @@ var loginCmd = &cobra.Command{
 func init() {
 	RootCmd.AddCommand(loginCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// loginCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// loginCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	loginCmd.Flags().StringVarP(&optLoginUsername, "username", "u", "", "User name to log in with")
 }
 
-func credentials() (string, string, error) {
+func loginPromptCredentials() (string, string, error) {
 	reader := bufio.NewReader(os.Stdin)
+	var err error
 
-	lastUsername := viper.GetString("last_username")
-	prompt := "Enter Username"
-	if lastUsername != "" {
-		prompt += fmt.Sprintf(" (empty for %s)", lastUsername)
+	var username string
+	if optLoginUsername != "" {
+		username = optLoginUsername
+	} else {
+		lastUsername := viper.GetString("last_username")
+		prompt := "Enter Username"
+		if lastUsername != "" {
+			prompt += fmt.Sprintf(" (empty for %s)", lastUsername)
+		}
+		prompt += ": "
+
+		fmt.Print(prompt)
+		username, err = reader.ReadString('\n')
+		username = strings.TrimSpace(username)
+		if username == "" && lastUsername != "" {
+			username = lastUsername
+		}
+
+		utils.SaveConfig("last_username", username)
+
+		if err != nil {
+			return "", "", err
+		}
 	}
-	prompt += ": "
 
-	fmt.Print(prompt)
-	username, err := reader.ReadString('\n')
-	username = strings.TrimSpace(username)
-	if username == "" && lastUsername != "" {
-		username = lastUsername
+	var bytePassword []byte
+	if term.IsTerminal(int(syscall.Stdin)) {
+		fmt.Print("Enter Password: ")
+		bytePassword, err = term.ReadPassword(int(syscall.Stdin))
+		// So that the next line of output doesn't overlap the password prompt's former spot
+		fmt.Println()
+	} else {
+		bytePassword, err = reader.ReadBytes('\n')
 	}
 
-	utils.SaveConfig("last_username", username)
-
-	if err != nil {
-		return "", "", err
-	}
-
-	fmt.Print("Enter Password: ")
-	bytePassword, err := term.ReadPassword(int(syscall.Stdin))
-	// So that the next line of output doesn't overlap the password prompt's former spot
-	fmt.Println()
 	if err != nil {
 		return "", "", err
 	}
